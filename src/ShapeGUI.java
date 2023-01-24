cd996c8f3f6d75b5632e65c0f3f2659adbbdd31e
alroney <andrewroney@rocketmail.com>
2023-01-23 19:22:34 -0500

    Change Log: ----------- - Added ActionListener Removal: Allows for the removal of ActionListeners     to prevent multiple instances of the same component. This prevents     displaying a different component from the one that was intended.
    - Added shapeDisplayPanel.removeAll(): Allows for the removal of all
        components from the shapeDisplayPanel. This prevents displaying a
        different component from the one that was intended. Now the
        shapeDisplayPanel will be cleared for the new component to be added.

--- src/ShapeGUI.java
+++ src/ShapeGUI.java
@@ -11,6 +11,7 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class ShapeGUI {
+    private static String input;
 
     //Method: create the GUI
     public static void createGUI() {
@@ -69,7 +70,7 @@ public class ShapeGUI {
         //region - 2D SHAPES INPUT - Create the input panels for each 2-dimensional shapes
             //region - CIRCLE INPUT PANEL - Create the input panel for the circle
                 JPanel circleInputPanel = new JPanel();//This is the panel for the users input for the circle
-                    Box horizontalBox = Box.createHorizontalBox();//This will create a horizontal box
+                    Box cipBox = Box.createHorizontalBox();//This will create a horizontal box
                     circleInputPanel.setLayout(new BoxLayout(circleInputPanel, BoxLayout.Y_AXIS));//This will set the layout of the panel to be vertical
                     circleInputPanel.setAlignmentX(Component.CENTER_ALIGNMENT);//This will align the panel to the center of the panel
                     circleInputPanel.setAlignmentY(Component.CENTER_ALIGNMENT);//This will align the panel to the top of the panel
@@ -77,31 +78,44 @@ public class ShapeGUI {
                     
                     //region - CIRCLE INPUT LABEL - Create the label for the users input for the circle
                         JLabel circleInputLabel = new JLabel("Enter the radius of the circle: ");//This is the label for the users input for the circle
-                        circleInputLabel.setAlignmentX(Component.LEFT_ALIGNMENT);//This will align the label to the left of the panel
-                        circleInputPanel.add(circleInputLabel);//This will add the label to the panel
+                            circleInputLabel.setAlignmentX(Component.LEFT_ALIGNMENT);//This will align the label to the left of the panel
+                            circleInputPanel.add(circleInputLabel);//This will add the label to the panel
                     //endregion
 
                     //region - CIRCLE INPUT TEXT FIELD - Create the text field for the users input for the circle
                         JTextField circleInputTextField = new JTextField();//This is the text field for the users input for the circle
-                        circleInputTextField.setMinimumSize(new Dimension(100, 25));//This will set the size of the text field
-                        circleInputTextField.setMaximumSize(new Dimension(100, 25));//This will set the size of the text field
-                        circleInputPanel.add(circleInputTextField);//This will add the text field to the panel
+                            circleInputTextField.setMinimumSize(new Dimension(100, 25));//This will set the size of the text field
+                            circleInputTextField.setMaximumSize(new Dimension(100, 25));//This will set the size of the text field
+                            circleInputPanel.add(circleInputTextField);//This will add the text field to the panel
                     //endregion
-                horizontalBox.add(circleInputLabel);//This will add the label to the horizontal box
-                horizontalBox.add(circleInputTextField);//This will add the text field to the horizontal box
-                circleInputPanel.add(horizontalBox);//This will add the horizontal box to the panel
+                cipBox.add(circleInputLabel);//This will add the label to the horizontal box
+                cipBox.add(circleInputTextField);//This will add the text field to the horizontal box
+                circleInputPanel.add(cipBox);//This will add the horizontal box to the panel
             //endregion
 
             //region - SQUARE INPUT PANEL - Create the input panel for the square
                 JPanel squareInputPanel = new JPanel();//This is the panel for the users input for the square
+                    Box sipBox = Box.createHorizontalBox();//This will create a horizontal box
+                    squareInputPanel.setLayout(new BoxLayout(squareInputPanel, BoxLayout.Y_AXIS));//This will set the layout of the panel to be vertical
+                    squareInputPanel.setAlignmentX(Component.CENTER_ALIGNMENT);//This will align the panel to the center of the panel
+                    squareInputPanel.setAlignmentY(Component.CENTER_ALIGNMENT);//This will align the panel to the top of the panel
+                    squareInputPanel.setMaximumSize(new Dimension(300, 150));//This will set the size of the panel
 
                     //region - SQUARE INPUT LABEL - Create the label for the users input for the square
                         JLabel squareInputLabel = new JLabel("Enter the length of the square: ");//This is the label for the users input for the square
+                            squareInputLabel.setAlignmentX(Component.LEFT_ALIGNMENT);//This will align the label to the left of the panel
+                            squareInputPanel.add(squareInputLabel);//This will add the label to the panel
                     //endregion
 
                     //region - SQUARE INPUT TEXT FIELD - Create the text field for the users input for the square
                         JTextField squareInputTextField = new JTextField(10);//This is the text field for the users input for the square
+                            squareInputTextField.setMinimumSize(new Dimension(100, 25));//This will set the size of the text field
+                            squareInputTextField.setMaximumSize(new Dimension(100, 25));//This will set the size of the text field
+                            squareInputPanel.add(squareInputTextField);//This will add the text field to the panel
                     //endregion
+                sipBox.add(squareInputLabel);//This will add the label to the horizontal box
+                sipBox.add(squareInputTextField);//This will add the text field to the horizontal box
+                squareInputPanel.add(sipBox);//This will add the horizontal box to the panel
             //endregion
 
             //region - RECTANGLE INPUT PANEL - Create the input panel for the rectangle
@@ -197,51 +211,67 @@ public class ShapeGUI {
                             triangleInputPanel.setVisible(false);//This makes the triangle input panel invisible to start with
 
         //endregion
-
-        dropdownCB.addActionListener(comboBoxListener -> {
-            if (comboBoxListener.getSource() instanceof JComboBox) {
-                //Get the selected item from the dropdown menu.
+        ActionListener[] dropdownCBListeners = dropdownCB.getActionListeners();
+        for (ActionListener listener : dropdownCBListeners) {
+            dropdownCB.removeActionListener(listener);
+        }
+        
+        dropdownCB.addActionListener(ShapeSelectionListener -> {
+            
+            if (ShapeSelectionListener.getSource() instanceof JComboBox) {
+                ActionListener[] displayShapeButtonListeners = displayShapeButton.getActionListeners();
+                for (ActionListener listener : displayShapeButtonListeners) {
+                    displayShapeButton.removeActionListener(listener);
+                }
                 String selectedShape = (String) dropdownCB.getSelectedItem();
-                errorMessage.setVisible(false);//This makes the error message invisible when the dropdown menu is changed
-                displayShapeButton.setVisible(false);//This makes the display shape button invisible when the dropdown menu is changed
-                inputPanel.setVisible(false);//This makes the input panel invisible when the dropdown menu is changed
-                circleInputPanel.setVisible(false);//This makes the circle input panel invisible to start with
-                squareInputPanel.setVisible(false);//This makes the square input panel invisible to start with
-                rectangleInputPanel.setVisible(false);//This makes the rectangle input panel invisible to start with
-                triangleInputPanel.setVisible(false);//This makes the triangle input panel invisible to start with
+                inputPanel.setVisible(false);//This will make the input panel invisible
+                errorMessage.setVisible(false);//This will make the error message invisible
+                shapeInputPanel.removeAll();//This will remove all the components from the shape input panel
+                circleInputPanel.setVisible(false);//This will make the circle input panel invisible
+                squareInputPanel.setVisible(false);//This will make the square input panel invisible
+                rectangleInputPanel.setVisible(false);//This will make the rectangle input panel invisible
+                triangleInputPanel.setVisible(false);//This will make the triangle input panel invisible
+                displayShapeButton.setVisible(false);//This will make the display shape button invisible
+                shapeDisplayPanel.setVisible(false);//This will make the shape display panel invisible
+                shapeDisplayPanel.removeAll();//This will remove all the components from the shape display panel
 
                 //region - SWITCH STATEMENT - This is the switch statement for the dropdown menu
                     switch(selectedShape) {
                         case "Circle":
-                            inputPanel.setVisible(true);
-                            shapeInputPanel.setVisible(true);
-                            circleInputPanel.setVisible(true);
-                            displayShapeButton.setVisible(true);
-                            //Create the button listener for the display shape button
-                            displayShapeButton.addActionListener(buttonListener -> {
-                                if (buttonListener.getSource() instanceof JButton) {
+                            inputPanel.setVisible(true);//This will make the input panel visible
+                            shapeInputPanel.setVisible(true);//This will make the shape input panel visible
+                            shapeInputPanel.add(circleInputPanel);//This will add the circle input panel to the shape input panel
+                            circleInputPanel.setVisible(true);//This will make the circle input panel visible
+                            displayShapeButton.setVisible(true);//This will make the display shape button visible
+                            displayShapeButton.addActionListener(CircleListener -> {//This is the action listener for the display shape button
+                                if (CircleListener.getSource() instanceof JButton) {
                                     try{
-                                        String input = circleInputTextField.getText();//This is the input from the text field
+                                        input = circleInputTextField.getText();//This is the input from the text field
                                         int inputChecked = checkInput(input);//This is the checked input from the text field
                                         switch(inputChecked){
                                             case 0:
+                                                shapeDisplayPanel.setVisible(false);
                                                 errorMessage.setText(getErrorMessage("e1"));
                                                 errorMessage.setVisible(true);
                                                 break;
                                             case 1:
+                                                shapeDisplayPanel.setVisible(false);
                                                 errorMessage.setText(getErrorMessage("e2"));
                                                 errorMessage.setVisible(true);
                                                 break;
                                             case 2:
+                                                shapeDisplayPanel.setVisible(false);
                                                 errorMessage.setText(getErrorMessage("e3"));
                                                 errorMessage.setVisible(true);
                                                 break;
                                             case 3:
+                                                shapeDisplayPanel.setVisible(false);
                                                 errorMessage.setText(getErrorMessage("e4"));
                                                 errorMessage.setVisible(true);
                                                 break;
                                             case 4:
                                                 errorMessage.setVisible(false);
+                                                shapeDisplayPanel.setVisible(true);
                                                 shapeDisplayPanel.removeAll();//This removes all the components from the shape display panel
                                                 shapeDisplayPanel.add(circleDisplay);//This adds the circle display to the shape display panel
                                                 circleDisplay.setRadius(Integer.parseInt(input));//This sets the radius of the circle display
@@ -249,6 +279,7 @@ public class ShapeGUI {
                                                 circleDisplay.repaint();//This repaints the circle display
                                                 break;
                                             default:
+                                                shapeDisplayPanel.setVisible(false);
                                                 errorMessage.setText(getErrorMessage("e5"));
                                                 errorMessage.setVisible(true);
                                                 break;
@@ -257,14 +288,79 @@ public class ShapeGUI {
                                         errorMessage.setText(getErrorMessage("e5"));
                                         errorMessage.setVisible(true);
                                     }
-                                    
-
-                                    
-                                    shapeDisplayPanel.revalidate();//This revalidates the shape display panel
-                                    shapeDisplayPanel.repaint();//This repaints the shape display panel
                                 }
+                                
                             });
                             break;
+
+                        case "Square":
+                            inputPanel.setVisible(true);
+                            shapeInputPanel.setVisible(true);
+                            shapeInputPanel.add(squareInputPanel);
+                            squareInputPanel.setVisible(true);
+                            displayShapeButton.setVisible(true);
+                            //Create the button listener for the display shape button
+                            displayShapeButton.addActionListener(SquareListener -> {
+                                if (SquareListener.getSource() instanceof JButton) {
+
+                                    try{
+                                        input = squareInputTextField.getText();//This is the input from the text field
+                                        int inputChecked = checkInput(input);//This is the checked input from the text field
+                                        switch(inputChecked){
+                                            case 0:
+                                                shapeDisplayPanel.setVisible(false);
+                                                errorMessage.setText(getErrorMessage("e1"));
+                                                errorMessage.setVisible(true);
+                                                break;
+                                            case 1:
+                                                shapeDisplayPanel.setVisible(false);
+                                                errorMessage.setText(getErrorMessage("e2"));
+                                                errorMessage.setVisible(true);
+                                                break;
+                                            case 2:
+                                                shapeDisplayPanel.setVisible(false);
+                                                errorMessage.setText(getErrorMessage("e3"));
+                                                errorMessage.setVisible(true);
+                                                break;
+                                            case 3:
+                                                shapeDisplayPanel.setVisible(false);
+                                                errorMessage.setText(getErrorMessage("e4"));
+                                                errorMessage.setVisible(true);
+                                                break;
+                                            case 4:
+                                                errorMessage.setVisible(false);
+                                                shapeDisplayPanel.setVisible(true);
+                                                shapeDisplayPanel.removeAll();//This removes all the components from the shape display panel
+                                                shapeDisplayPanel.add(squareDisplay);//This adds the square display to the shape display panel
+                                                squareDisplay.setSide(Integer.parseInt(input));//This sets the side of the square display
+                                                squareDisplay.revalidate();//This revalidates the square display
+                                                squareDisplay.repaint();//This repaints the square display
+                                                break;
+                                            default:
+                                                shapeDisplayPanel.setVisible(false);
+                                                errorMessage.setText(getErrorMessage("e5"));
+                                                errorMessage.setVisible(true);
+                                                break;
+                                        }
+                                    }catch(Exception e){
+                                        errorMessage.setText(getErrorMessage("e5"));
+                                        errorMessage.setVisible(true);
+                                    }
+                                }
+                                
+                            });
+                            break;
+
+                            default:
+                                inputPanel.setVisible(false);
+                                shapeInputPanel.setVisible(false);
+                                circleInputPanel.setVisible(false);
+                                squareInputPanel.setVisible(false);
+                                displayShapeButton.setVisible(false);
+                                shapeDisplayPanel.setVisible(false);
+                                errorMessage.setVisible(false);
+                                break;
+                            
                     }
                 //endregion
             }
@@ -286,8 +382,8 @@ public class ShapeGUI {
 
     //Create method to check if a string is non-numeric (contains alphabetical characters), empty, less than 0 (contains a '-' or is 0), or greater than 200. Reuturn a int to indicate which error it is.
     private static int checkInput(String input){
-        if(!input.matches("-?\\d+(\\.\\d+)?")) return 0;
         if(input.isEmpty()) return 1;
+        if(!input.matches("-?\\d+(\\.\\d+)?")) return 0;
         if(Integer.parseInt(input) <= 0) return 2;
         if(Integer.parseInt(input) > 200) return 3;
         return 4;
